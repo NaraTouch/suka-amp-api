7ef935a9cf75b00abc5c06e347628eaf6bf34c73
NaraTouch <sophonaratouch@gmail.com>
2022-04-21 10:59:15 +0700

    deploy v1

---
+++ dist/modules/posts/posts.providers.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.postsProviders = void 0;
+const post_entity_1 = require("./post.entity");
+const constants_1 = require("../../core/constants");
+exports.postsProviders = [
+    {
+        provide: constants_1.POST_REPOSITORY,
+        useValue: post_entity_1.Post,
+    },
+];
+//# sourceMappingURL=posts.providers.js.map
